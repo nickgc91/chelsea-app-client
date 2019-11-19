c50b2de94db9b8c386e96dbda3c10e30e3e72a32
Nicolas Charlet <nicolas@nicolascharlet.com>
2019-11-19 20:58:17 +0000

    team component with stats done and started creating fixtures and table components

---
+++ client/src/components/Fixtures.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/fixtures.css";
+
+class Fixtures extends React.Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="fixtures-grid">
+          <h1>fixtures</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Fixtures;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,132 +1,66 @@
 import React from "react";
 import "../css/home.css";
+import Team from "./Team"
+import Fixtures from "./Fixtures"
 
 class Home extends React.Component {
   state = {
-    kepaInfo: false
-  };
+    showFixtures: true,
+    showTeam: false,
+    showTable: false
+  }
 
-  changeKepa = () => {
+  onClickToggleShowFixtures = () => {
     this.setState({
-      kepaInfo: !this.state.kepaInfo
+      showFixtures: true,
+      showTeam: false,
+      showTable: false
     });
-  };
+  }
+
+  onClickToggleShowTable = () => {
+    this.setState({
+      showTable: true,
+      showFixtures: false,
+      showTeam: false,
+    });
+  }
+
+  onClickToggleShowTeam = () => {
+    this.setState({
+      showTeam: true,
+      showFixtures: false,
+      showTable: false
+    });
+  }
+
+
 
   render() {
     return (
+      <div style={{ textAlign: 'center', padding: '20px' }}>
+      <button
+      style={{ paddingBottom: '10px', width: '200px', color: 'white' }} 
+      className="title"
+      onClick={this.onClickToggleShowFixtures}>
+      <h3>Show Chelsea Fixtures</h3></button> 
+      <button
+      style={{ paddingBottom: '10px', width: '200px', color: 'white' }} 
+      className="title"
+      onClick={this.onClickToggleShowTable}>
+      <h3>Show Premier League Table</h3></button> 
+      <button
+      style={{ paddingBottom: '10px', width: '200px', color: 'white' }} 
+      className="title"
+      onClick={this.onClickToggleShowTeam}>
+      <h3>Display Starting 11</h3></button> 
       <div className="main-grid">
-        <div className="grid-container">
-          <h1 className="title">TEAM</h1>
-          {!this.state.kepaInfo ? (
-            <div className="keeper">
-              <img
-                onMouseEnter={this.changeKepa}
-                src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p109745.png"
-                alt="image"
-                style={{ width: "150px", height: "150px" }}
-              />
-              Kepa Arrizabalaga
-            </div>
-          ) : (
-            <div
-              onMouseLeave={this.changeKepa}
-              className="keeper"
-              style={{ width: "170px", height: "150px" }}
-            >
-              <img
-                onMouseEnter={this.changeKepa}
-                src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p109745.png"
-                alt="image"
-                style={{ width: "150px", height: "150px", opacity: 0.3 }}
-              />
-              12 APPEARANCES 
-              3 CLEAN SHEETS 
-              20 SAVES 
-              54% SHOTS SAVED
-            </div>
-          )}
-
-          <div className="wingBackLeft">
-            <img
-              src="https://chelseacore.com/wp-content/uploads/2018/10/Emerson-1.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            Emerson Palmieri
-          </div>
-          <div className="wingBackRight">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p41328.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            César Azpilicueta
-          </div>
-          <div className="centerBackRight">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p103192.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            Kurt Zouma
-          </div>
-          <div className="centerBackLeft">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p194794.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            Fikayo Tomori
-          </div>
-          <div className="defensiveMidfieldLeft">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p116594.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            N'Golo Kanté
-          </div>
-          <div className="defensiveMidfieldRight">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p85955.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            Jorginho
-          </div>
-          <div className="offensiveMidfieldRight">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p47431.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            Willian
-          </div>
-          <div className="offensiveMidfieldCenter">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p184341.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            Mason Mount
-          </div>
-          <div className="offensiveMidfieldLeft">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p176413.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            Christian Pulisic
-          </div>
-          <div className="striker">
-            <img
-              src="https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p173879.png"
-              alt="image"
-              style={{ width: "150px", height: "150px" }}
-            />
-            Tammy Abraham
-          </div>
-        </div>
+        
+        {this.state.showTeam ? 
+        <Team /> : null }
+        {this.state.showFixtures ? 
+        <Fixtures /> : null }
+      </div>
       </div>
     );
   }
