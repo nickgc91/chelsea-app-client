4bb93be5ba0a4e02f3c34f19f85ea3ac9cd201ba
Nicolas Charlet <nicolas@nicolascharlet.com>
2019-11-27 15:30:59 +0000

    added fixtures

--- client/src/components/Fixtures.js
+++ client/src/components/Fixtures.js
@@ -4,10 +4,71 @@ import "../css/home.css";
 class Fixtures extends React.Component {
   render() {
     return (
-          <div className="fixturesTable">
-          <h1>fixtures</h1>
-          </div>
-       
+      <div className="fixturesTable">
+        <ul>
+          <li>
+            Man United 4 - 0 Chelsea (PL)
+          </li>
+          <br/><br/>
+          <li>Liverpool 2 (5) - 2 (4) Chelsea (Uefa Super Cup)</li>
+          <br/><br/>
+          <li>Chelsea 1 - 1 Leicester City (PL)</li>
+          <br/><br/>
+          <li>Norwich City 2  - 3 Chelsea (PL)</li>
+          <br/><br/>
+          <li>Chelsea 2 - 2 Sheffield United (PL)</li>
+          <br/><br/>
+          <li>Wolves 2 - 5 Chelsea (PL)</li>
+          <br/><br/>
+          <li>Chelsea 0 - 1 Valencia (CL)</li>
+          <br/><br/>
+          <li>Chelsea 1 - 2 Liverpool (PL)</li>
+          <br/><br/>
+          <li>Chelsea 7 - 1 Grimsby Town (LC)</li>
+          <br/><br/>
+          <li>Chelsea 2 - 0 Brighton (PL)</li>
+          <br/><br/>
+          <li>Lille 1 - 2 Chelsea (CL)</li>
+          <br/><br/>
+          <li>Southampton 1 - 4 Chelsea (PL)</li>
+          <br/><br/>
+          <li>Chelsea 1 - 0 Newcastle (PL)</li>
+          <br/><br/>
+          <li>Ajax 0 - 1 Chelsea (CL)</li>
+          <br/><br/>
+          <li>Burnley 2 - 4 Chelsea (PL)</li>
+          <br/><br/>
+          <li>Chelsea 1 - 2 Man United (LC)</li>
+          <br/><br/>
+          <li>Watford 1 - 2 Chelsea (PL)</li>
+          <br/><br/>
+          <li>Chelsea 4 - 4 Ajax (CL)</li>
+          <br/><br/>
+          <li>Chelsea 2 - 0 Crystal Palace (PL)</li>
+          <br/><br/>
+          <li>Man. City 2 - 1 Chelsea (PL)</li>
+          <br/><br/>
+          <li>27/11/19 Valencia - Chelsea (CL)</li>
+          <br/><br/>
+          <li>30/11/19 Chelsea - West Ham (PL)</li>
+          <br/><br/>
+          <li>04/12/19 Chelsea - Aston Villa (PL)</li>
+          <br/><br/>
+          <li>07/12/19 Everton - Chelsea (PL)</li>
+          <br/><br/>
+          <li>10/12/19 Chelsea - Lille (CL)</li>
+          <br/><br/>
+          <li>14/12/19 Chelsea - Bournemouth (PL)</li>
+          <br/><br/>
+          <li>22/12/19 Tottenham - Chelsea (PL)</li>
+          <br/><br/>
+          <li>26/12/19 Chelsea - Southampton (PL)</li>
+          <br/><br/>
+          <li>29/12/19 Arsenal - Chelsea (PL)</li>
+          <br/><br/>
+          <li>01/01/2020 Brighton - Chelsea (PL)</li>
+        </ul>
+      </div>
     );
   }
 }
