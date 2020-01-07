f4d04b078b5c8fa95021de9c40c85604b07931c6
Nicolas Charlet <nicolas@nicolascharlet.com>
2020-01-07 18:39:17 +0000

    made updates to update page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,17 +2,36 @@ import React from "react";
 import "../css/login.css";
 
 class Login extends React.Component {
-  loginUser = (username, password) => {
-      console.log(username)
+
+  state= {
+    loggedIn: false,
+    showForm: true,
+    username: null
+  }
+
+  loginUser = e => { 
+    e.preventDefault()
+    this.setState({
+      loggedIn: true,
+      username: e.target.username.value,
+      showForm: false
+    })
+      e.target.username.value = ''
+      e.target.password.value = ''
   };
 
   render() {
     return (
       <div>
         <div className="loginForm">
+          {this.state.loggedIn ? 
+          <div>
+            <h1>Welcome back {this.state.username}</h1>
+          </div> : null }
+          {this.state.showForm ? 
           <div className="form">
             <form  onSubmit={(username, password) => this.loginUser(username, password)}>
-                <label >
+                <label>
                     Username:  
                     <input type='text' name='username'/>
                 </label>
@@ -21,8 +40,14 @@ class Login extends React.Component {
                     Password: 
                     <input type='password' name='password'/>
                 </label>
+                <br></br>
+                <br></br>
+                <button
+                onSubmit={e => this.loginUser(e)}>
+                  Submit
+                </button>
             </form>
-          </div>
+          </div> : null }
         </div>
       </div>
     );
