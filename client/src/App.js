87dbf639559a9cad495564d32fb4eda1f95430bf
Nicolas Charlet <nicolas@nicolascharlet.com>
2020-01-18 13:51:49 +0000

    working on auth frontend, loggin user in

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import './css/App.css'
-import { Switch, Route } from 'react-router-dom'
-import News from './components/News'
-import Home from './components/Home'
-import Login from './components/Login'
+import React from "react";
+import "./css/App.css";
+import { Switch, Route } from "react-router-dom";
+import News from "./components/News";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import FanProfile from "./components/FanProfile";
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path='/news'>
-        <News />
-      </Route>
-      <Route exact path='/login'>
-        <Login />
-      </Route>
-    </Switch>
-  );
+class App extends React.Component {
+  state = {
+    username: ""
+  };
+
+  signIn = username => {
+    this.setState({ username });
+  };
+
+  signOut = () => {
+    this.setState({ username: "" });
+  };
+
+  render() {
+    return (
+      <switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route
+          exact
+          path="/news"
+          component={routerProps => <News {...routerProps} />}
+        />
+        <Route
+          exact
+          path="/login"
+          component={routerProps => (
+            <Login {...routerProps} signIn={this.signIn} />
+          )}
+        />
+        <Route
+          exact
+          path="/profile"
+          component={routerProps => (
+            <FanProfile {...routerProps}  />
+          )}
+        />
+      </switch>
+    );
+  }
 }
 
 export default App;
